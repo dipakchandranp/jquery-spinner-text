9a5732be0f05ef3c0763fc2e44317bb78486d1f8
Dipak Chandran P <dipakchandranp15@gmail.com>
2016-01-16 18:50:46 +0530

    index updated.

--- js/custom_script.js
+++ js/custom_script.js
@@ -1,24 +1,59 @@
 $(function(){
-	
+	var heading = "jQuery Spinner Text.JS";
+	t = [15];
+	t[1] = "&lt!-- load jquery plugin --&gt;",
+	t[2] = '&lt;script src="js/jquery.min.js"&gt;&lt;/script&gt;',
+	t[3] = '&lt!-- load jquery-spinner-text.js plugin --&gt;',
+	t[4] = '&lt;script src="js/jquery-spinner-text.js"&gt;&lt;/script&gt;',
+	t[5] = '..........',
+	t[6] = '$("#heading"").start_spinwriter({',
+	t[7]= '"text" : "SPINNER TEXT",',
+	t[8] = '"interval" : 10,//milliseconds',
+	t[9] = '"css" : {',
+	t[10] = "    'font-size': '26px',",
+	t[11] = "    'text-transform': 'uppercase',",
+	t[12] = "    'color':'#333'",
+	t[13] = '  }',
+	t[14] = ' });';
+
 
 	// Initiate plugin
 	$('#heading').start_spinwriter({
-		'text' : "SPINNER TEXT",
+		'text' : heading,
 		'interval' : 10,//milliseconds
 		'css' : {
-			'font-size': '26px',
-    		'text-transform': 'uppercase',
     		'color':'#333'
 		}
 	});
 
-	// 	heading_small
-	$('#heading_small').start_spinwriter({
-		'text' : "Welcome to spinner text jQuery plugin.",
-		'interval' : 500,//milliseconds
-		'css' : {
+	load_texts(1);
+
+	function load_texts(i){
+		if( i < 15){
+			$('#t'+i).start_spinwriter({
+				'text' : t[i],
+				'interval':2,
+				'css':{}
+			},function(){
+				i = i + 1;
+				load_texts(i);
+			});
+		}else{
+			setTimeout(function(){
+				$('pre>span').html('');
+				$('pre>p').html('');
+				load_texts(1);		
+			},100000)
+			
 		}
-	});
+	}
+	// 	heading_small
+// 	$('#heading_small').start_spinwriter({
+// 		'text' : "Welcome to spinner text jQuery plugin.",
+// 		'interval' : 10,//milliseconds
+// 		'css' : {
+// 		}
+// 	});
 
 });
 
--- js/jquery-spinner-text.js
+++ js/jquery-spinner-text.js
@@ -1,25 +1,28 @@
 (function ( $ ) {
-    $.fn.start_spinwriter = function(args){
+    $.fn.start_spinwriter = function(args,callback){
 		this.css(args.css);
 		text = args.text.split('');
 		interval = args.interval;
-		write_text(this,text,interval);
+		var rt = 404;
+		write_text(this,text,interval,callback);
 		return this;
 	}
 
-	write_text = function(ele,text,interval){
-		write_next_char(ele,text,1,interval)
+	write_text = function(ele,text,interval,callback){
+		return write_next_char(ele,text,1,interval,callback)
 	}
 
-	write_next_char = function(ele,text,index,interval){
+	write_next_char = function(ele,text,index,interval,callback){
 		if(index < text.length+1){
-			load_current_character(ele, text.slice(0,index).join(''),0,text,index,interval);
+			load_current_character(ele, text.slice(0,index).join(''),0,text,index,interval,callback);
 		}else{
-			return;
+			if(callback){
+			 	callback();	
+			}
 		}
 	}
 
-	load_current_character = function(ele,str,posi, text,index,interval){
+	load_current_character = function(ele,str,posi, text,index,interval,callback){
 		if(str.length > 0){
 			var alphabet = " abcdefghijklmnopqrstuvwxyz".split("");
 			pt = /[a-zA-Z]/;
@@ -28,13 +31,12 @@
 			if(!pt.test(running_charecter) || alphabet[posi].toLowerCase() == running_charecter.toLowerCase()){
 				ele.html(str);
 				index = index + 1;
-				write_next_char(ele,text,index,interval);
-				return;
+				write_next_char(ele,text,index,interval,callback);
 			}else{
 				ele.html(main_str+alphabet[posi]);	
 				setTimeout(function(){
 					posi = posi + 1;
-					load_current_character(ele,str,posi,text,index,interval);
+					load_current_character(ele,str,posi,text,index,interval,callback);
 				},interval);
 			}
 		}
